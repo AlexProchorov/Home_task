26a135835b3a247f30f937330230507a8d0fddd0
ALex <proch0r0v@yandex.ru>
2020-11-19 13:11:23 +0300

    clean repo

--- HomeWork 2.5.py
+++
@@ -1,6 +0,0 @@
-my_list = [7, 5, 3, 3, 2,9]
-
-user_answer=int(input('Введите число рейтинг-таблицы: '))
-my_list.append((user_answer))
-my_list.sort(reverse=True)
-print(my_list)
--- My_homework 1.6.py
+++
@@ -1,9 +0,0 @@
-first_result=int(input("Введите результат первого дня пробежки в КМ: "))
-aim_result=int(input("Введите желаемый результат в КМ: "))
-
-day = 0
-
-while first_result < aim_result:
-    first_result = first_result * 1.1
-    day += 1
-print(day)
